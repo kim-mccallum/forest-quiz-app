5f30f975e8d6343c8e57f836385b81916e746257
Kim McCallum <kchaase@gmail.com>
2019-12-10 19:25:40 -0700

    Added basic HTML, a few styles, and questions in the JS. Images added too

---
+++ index.js
@@ -0,0 +1,29 @@
+// Move this to a separate js file later (STORE.js) for manageability
+// Question database (array of objects)
+const STORE = [
+   {question:"What proportion of the world’s land area is covered by forest and what proportion of land-based plan and animal species do forests support?",
+    options: ["One third of land cover and over half of land species.","Two thirds of land cover and less than half of land species.","Ten percent of land cover and one quarter of land  species."],
+    answer: "One third of land cover and over half of land species.",
+    displayAnswer: "Forests cover one-third of the world’s land area and host more than half of the world’s land-based plant and animal species.",
+    source: "http://www.fao.org/zhc/detail-events/en/c/1033884/"},
+
+    {question:"Which of the following statements describes the role of forests in global climate?",
+    options: ["Forests do not play a significant role in the global carbon cycle.","Trees are a carbon sink and the world’s forests remove an estimated 2.1 Gigatonnes of carbon dioxide annually.","Trees are a significant source of greenhouse gas, producing large amounts of carbon dioxide."],
+    answer: "Trees are a carbon sink and the world’s forests remove an estimated 2.1 Gigatonnes of carbon dioxide annually.",
+    displayAnswer: "Trees are a great carbon sink, with the world’s forests removing an estimated 2.1 Gigatonnes of carbon dioxide annually – that’s 2.1 billion tonnes! This plays a fundamental role in balancing the world’s carbon cycle and helping to combat climate change.",
+    source: "http://www.fao.org/zhc/detail-events/en/c/1033884/"},
+
+   {question:"Which of the following statements describes another important role of forests?",
+    options: ["Forests lower the water table significantly, creating wetland habitat.","Forests reduce drought effects by retaining water, preventing run-off and mitigating flood damage.","Forests create natural habitat barriers, preventing the spread of native species."],
+    answer: "Forests reduce drought effects by retaining water, preventing run-off and mitigating flood damage.",
+    displayAnswer: "Forests can retain excess rainwater, prevent extreme run-offs and reduce the damage of flooding. They can also help mitigate the effects of droughts by releasing water in the dry season, forests can also help provide clean water and mitigate the effects of droughts",
+    source: "https://www.un-redd.org/forest-facts"},
+    // MANY MORE QUESTIONS TO COME
+];
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
